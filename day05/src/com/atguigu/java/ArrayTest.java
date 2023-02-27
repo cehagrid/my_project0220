f202efc449ab1749bdbb547481cfd5623f1f8706
Layne <Layne@atguigu.com>
2023-02-27 12:10:35 +0800

    first day05_23-2-27

--- day05/src/com/atguigu/java/ArrayTest.java
+++ day05/src/com/atguigu/java/ArrayTest.java
@@ -2,7 +2,10 @@ package com.atguigu.java;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 public class ArrayTest {
+    //for循环遍历数组
     @Test
     public void test(){
         int arr1[] = new int[3];
@@ -26,4 +29,42 @@ public class ArrayTest {
         System.out.println(arr5[1][1]);
 
     }
+
+    /**
+     * 1.练习1:用一个数组存储26个小写英文字母，并遍历显示，显示要求如: a->A
+     */
+    @Test
+    public void test1(){
+        char[] letters = new char[26];
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = (char)(i+97);
+        }
+        for (int i = 0; i < letters.length; i++) {
+            System.out.println(letters[i]+"->"+(char)(i+65));
+        }
+    }
+
+    /**
+     * 2.练习3:用一个数组存储本组学员的姓名，从键盘输入，并遍历显示
+     */
+    @Test
+    public void test2(){
+        String[] name = new String[3];
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < name.length; i++) {
+            System.out.println("请输入姓名：");
+            name[i] = scan.nextLine();
+        }
+        //方式一：普通for循环
+        for (int i = 0; i <name.length; i++) {
+            System.out.print(name[i]+" ");
+        }
+        System.out.println("-------------------");
+        //方式2：增强for循环
+        for (String str : name) {
+            System.out.print(str+" ");
+        }
+    }
+    
+
 }
