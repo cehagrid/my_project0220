e4630db4b4ff47d734cbd014104ec1cff1db556a
Layne <Layne@atguigu.com>
2023-02-28 19:48:45 +0800

    ninth commit

--- day06ArrayAndMethod/src/com/atguigu/method/ReflectionTest.java
+++ day06ArrayAndMethod/src/com/atguigu/method/ReflectionTest.java
@@ -3,24 +3,38 @@ package com.atguigu.method;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ReflectionTest {
     @Test
-    public void test() throws Exception {
-        ClassLoader classLoader = ReflectionTest.class.getClassLoader();
-        Class<?> clazz = classLoader.loadClass("com.atguigu.method.ReflectionTest");
+    public void test() {
+        boolean invoke = false;
+        try {
+            ClassLoader classLoader = ReflectionTest.class.getClassLoader();
+            Class<?> clazz = classLoader.loadClass("com.atguigu.method.ReflectionTest");
 
-        //1.创建构造器,创建对象（可以带参，更普适，构造器就是对象）
-        Constructor<?> constructor = clazz.getDeclaredConstructor();
-        //2.
-        constructor.setAccessible(true);
-        //3.构造器传参数
-        ReflectionTest reflectionTest = (ReflectionTest) constructor.newInstance();
+            //1.创建构造器,创建对象（可以带参，更普适，构造器就是对象）
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            //2.
+            constructor.setAccessible(true);
+            //3.构造器传参数
+            ReflectionTest reflectionTest = (ReflectionTest) constructor.newInstance();
 
-        Method isPrime = clazz.getDeclaredMethod("isPrime", int.class);
-        isPrime.setAccessible(true);
-        boolean invoke = (boolean) isPrime.invoke(reflectionTest, 3);
+            Method isPrime = clazz.getDeclaredMethod("isPrime", int.class);
+            isPrime.setAccessible(true);
+            invoke = (boolean) isPrime.invoke(reflectionTest, 3);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(invoke);
     }
 
