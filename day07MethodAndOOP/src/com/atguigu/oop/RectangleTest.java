bff5e1f726f5a11a4c485053d318cb47f535e545
Layne <Layne@atguigu.com>
2023-03-01 17:12:01 +0800

    forth commit

--- day07MethodAndOOP/src/com/atguigu/oop/RectangleTest.java
+++ day07MethodAndOOP/src/com/atguigu/oop/RectangleTest.java
@@ -2,9 +2,23 @@ package com.atguigu.oop;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class RectangleTest {
     @Test
-    public void test(){
+    public void test() throws Exception {
+        ClassLoader classLoader = Rectangle.class.getClassLoader();
+        Class<?> clazz = classLoader.loadClass("com.atguigu.oop.Rectangle");
+
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Rectangle r = (Rectangle) constructor.newInstance();
+
+        Method toString = clazz.getDeclaredMethod("toString",null);
+        toString.setAccessible(true);
+        toString.invoke(r,null);
+
         Rectangle rectangle = new Rectangle();
         System.out.println(rectangle);
     }
