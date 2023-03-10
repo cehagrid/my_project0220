c162912e60c6eae871a3af4541e898e779b575e6
Layne <Layne@atguigu.com>
2023-03-10 21:00:37 +0800

    sixth commit

--- homework/src/com/atguigu/day13/TotalWord.java
+++ homework/src/com/atguigu/day13/TotalWord.java
@@ -5,21 +5,25 @@ import org.junit.Test;
 public class TotalWord {
     @Test
     public void test() {
-        String words = "hello123world";
+        String words = "helloworld";
 
         //创建包含所有可能的数组统计字符串出现的次数,下标为字符
         int[] counts = new int[26];
+        String s;
         for (int i = 0; i < words.length(); i++) {
-            counts[words.charAt(i) - 'a']++;
+            //大写转小写,
+            s = words.toLowerCase();
+            counts[s.charAt(i) - 'a']++;
         }
 
         //遍历统计数组
+        char letter = 'a';
         for (int i = 0; i < counts.length; i++) {
-            System.out.println(counts[i]);
+            System.out.println(letter+++":"+counts[i]);
         }
     }
 
-    String ss = "jaj";
+
 
     @Test
     public void test1() {
